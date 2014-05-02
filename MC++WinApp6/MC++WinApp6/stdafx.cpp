7f1cc4920cc962aa6c66c5c4b94cbb4c204346e1
BlackySoul <indarklight@gmail.com>
2014-05-02 17:45:34 +0700

    Удаление
    deldel

--- MC++WinApp6/MC++WinApp6/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// MC++WinApp6.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
